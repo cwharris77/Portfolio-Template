95cd79314abf8c845296953c1858a158baa139ff
Cooper Harris <cwharris365@gmail.com>
2025-08-23 18:00:24 -0700

    Update dates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,9 @@
-import { motion } from 'framer-motion';
-import PropTypes from 'prop-types'; // Add this line to import PropTypes
-import { services } from '../constants';
-import { SectionWrapper } from '../hoc';
-import { styles } from '../styles';
-import { fadeIn, textVariant } from '../utils/motion';
-
+import { motion } from "framer-motion";
+import PropTypes from "prop-types"; // Add this line to import PropTypes
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => {
   // Add prop type validation for 'index'
@@ -13,14 +12,15 @@ const ServiceCard = ({ index, title, icon }) => {
     title: PropTypes.string.isRequired,
     icon: PropTypes.string.isRequired,
   };
-  
+
   return (
     <motion.div
-      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
-      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
-      <div className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
-        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
-        <h3 className="text-taupe text-[18px] font-bold text-center">
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className='xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div className='bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
+        <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+        <h3 className='text-taupe text-[18px] font-bold text-center'>
           {title}
         </h3>
       </div>
@@ -30,26 +30,28 @@ const ServiceCard = ({ index, title, icon }) => {
 
 const About = () => {
   return (
-    <div className="-mt-[9rem]">
+    <div className='-mt-[9rem]'>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}></h2>
       </motion.div>
 
       <motion.p
-        variants={fadeIn('', '', 0.1, 1)}
-        className="mt-4 text-taupe text-[18px] max-w-6xl leading-[30px]">
-          As a full-stack software engineer at Snap Inc. on the Business 
-          Experience team, I focus on building and maintaining end-to-end solutions 
-          that improve tools for businesses on the platform. I work on both front-end 
-          interfaces and back-end systems, ensuring that business partners and advertisers 
-          have a seamless and efficient experience. Collaborating closely with product managers, 
-          designers, and other engineering teams, I help deliver scalable, reliable, and 
-          high-performance features that optimize how businesses interact with Snap&apos;s 
-          services.
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-taupe text-[18px] max-w-6xl leading-[30px]'
+      >
+        As a former full-stack software engineer at Snap Inc. on the Business
+        Experience team, I focused on building and maintaining end-to-end
+        solutions that improved tools for businesses on the platform. I worked
+        on both front-end interfaces and back-end systems, ensuring that
+        business partners and advertisers had a seamless and efficient
+        experience. Collaborating closely with product managers, designers, and
+        other engineering teams, I helped deliver scalable, reliable, and
+        high-performance features that optimized how businesses interacted with
+        Snap&#39;s services.
       </motion.p>
 
-      <div className="mt-20 flex flex-wrap gap-10">
+      <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
@@ -58,6 +60,6 @@ const About = () => {
   );
 };
 
-const AboutSection = SectionWrapper(About, 'about');
+const AboutSection = SectionWrapper(About, "about");
 
 export default AboutSection;
--- src/constants/index.js
+++ src/constants/index.js
@@ -24,252 +24,252 @@ import {
   typescript,
   uofa,
   ux,
-  wiki
-} from '../assets';
+  wiki,
+} from "../assets";
 
 export const navLinks = [
   {
-    id: 'about',
-    title: 'About',
+    id: "about",
+    title: "About",
   },
   {
-    id: 'projects',
-    title: 'Projects',
+    id: "projects",
+    title: "Projects",
   },
   {
-    id: 'contact',
-    title: 'Contact',
+    id: "contact",
+    title: "Contact",
   },
 ];
 
 const services = [
   {
-    title: 'Front-End Engineering',
+    title: "Front-End Engineering",
     icon: frontend,
   },
   {
-    title: 'UI/UX Design',
-    icon: ux
+    title: "UI/UX Design",
+    icon: ux,
   },
   {
-    title: 'Back-End Engineering',
+    title: "Back-End Engineering",
     icon: backend,
   },
   {
-    title: 'iOS Mobile Development',
+    title: "iOS Mobile Development",
     icon: apple,
   },
 ];
 
 const technologies = [
   {
-    name: 'React JS',
+    name: "React JS",
     icon: reactjs,
   },
   {
-    name: 'TypeScript',
+    name: "TypeScript",
     icon: typescript,
   },
   {
-    name: 'GraphQL',
+    name: "GraphQL",
     icon: graphql,
   },
   {
-    name: 'Apollo Client',
+    name: "Apollo Client",
     icon: apollo,
   },
   {
-    name: 'Jest',
+    name: "Jest",
     icon: jest,
   },
   {
-    name: 'Java',
+    name: "Java",
     icon: java,
   },
   {
-    name: 'Emotion JS',
+    name: "Emotion JS",
     icon: emotion,
   },
   {
-    name: 'Git',
+    name: "Git",
     icon: git,
   },
   {
-    name: 'Swift',
+    name: "Swift",
     icon: swift,
   },
   {
-    name: 'Express JS',
+    name: "Express JS",
     icon: express,
   },
   {
-    name: 'Python3',
+    name: "Python3",
     icon: python,
   },
   {
-    name: 'C',
+    name: "C",
     icon: c,
   },
   {
-    name: 'Node JS',
+    name: "Node JS",
     icon: nodejs,
   },
   {
-    name: 'AWS',
+    name: "AWS",
     icon: aws,
   },
 ];
 
 const experiences = [
   {
-    title: 'Computer Science Student',
-    company_name: 'University of Arizona',
+    title: "Computer Science Student",
+    company_name: "University of Arizona",
     icon: uofa,
-    iconBg: '#001a5d',
-    date: 'August 2020 - May 2024',
+    iconBg: "#001a5d",
+    date: "August 2020 - May 2024",
   },
   {
-    title: 'Student Technology Assistant',
-    company_name: `Housing & Residential Life ${'@'} University of Arizona`,
+    title: "Student Technology Assistant",
+    company_name: `Housing & Residential Life ${"@"} University of Arizona`,
     icon: uofa,
-    iconBg: '#e2002d',
-    date: 'June 2022 - May 2024',
+    iconBg: "#e2002d",
+    date: "June 2022 - May 2024",
   },
   {
-    title: 'AI Reviewer',
-    company_name: 'Remotetasks',
+    title: "AI Reviewer",
+    company_name: "Remotetasks",
     icon: remotetasks,
-    iconBg: '#35ce9d',
-    date: 'December 2023 - June 2024',
+    iconBg: "#35ce9d",
+    date: "December 2023 - June 2024",
   },
   {
-    title: 'Software Engineer',
-    company_name: 'Snap Inc.',
+    title: "Software Engineer",
+    company_name: "Snap Inc.",
     icon: snapchat,
-    iconBg: '#FFFC00',
-    date: 'August 2024 - Present',
+    iconBg: "#FFFC00",
+    date: "August 2024 - June 2025",
   },
 ];
 
 const projects = [
   {
-    id: 'project-1',
-    name: 'CyberTRIP',
-    description: 'A web-based tool designed to simplify cybersecurity incident tracking, management, and prioritization in real time.',
+    id: "project-1",
+    name: "CyberTRIP",
+    description:
+      "A web-based tool designed to simplify cybersecurity incident tracking, management, and prioritization in real time.",
     tags: [
       {
-        name: 'Flask',
-        color: 'white',
+        name: "Flask",
+        color: "white",
       },
       {
-        name: 'MongoDB',
-        color: 'green-text-gradient',
+        name: "MongoDB",
+        color: "green-text-gradient",
       },
       {
-        name: 'Web Development',
-        color: 'pink-text-gradient',
+        name: "Web Development",
+        color: "pink-text-gradient",
       },
     ],
     info: "",
     image: cybertrip,
-    repo: 'https://github.com/cwharris77/cyberTRIP',
-    demo: '',
+    repo: "https://github.com/cwharris77/cyberTRIP",
+    demo: "",
   },
   {
-    id: 'project-2',
-    name: 'ReeldIn',
+    id: "project-2",
+    name: "ReeldIn",
     description: `A website dedicated to personally tailoring your movie selection experience! `,
     tags: [
       {
-        name: 'Django',
-        color: 'blue-text-gradient',
+        name: "Django",
+        color: "blue-text-gradient",
       },
       {
-        name: 'PostgreSQL',
-        color: 'green-text-gradient',
+        name: "PostgreSQL",
+        color: "green-text-gradient",
       },
       {
-        name: 'Web Development',
-        color: 'pink-text-gradient',
+        name: "Web Development",
+        color: "pink-text-gradient",
       },
     ],
     info: "This project was accomplished as part of an Agile team",
     image: reeldin,
-    repo: 'https://github.com/jonathanhouge/ReeldIn',
-    demo: '',
+    repo: "https://github.com/jonathanhouge/ReeldIn",
+    demo: "",
   },
   {
-    id: 'project-3',
-    name: 'Alien Invasion',
+    id: "project-3",
+    name: "Alien Invasion",
     description:
-      'A version of the classic Alien Invasion game created using pygame.',
+      "A version of the classic Alien Invasion game created using pygame.",
     tags: [
       {
-        name: 'Python3',
-        color: 'blue-text-gradient',
+        name: "Python3",
+        color: "blue-text-gradient",
       },
       {
-        name: 'Pygame',
-        color: 'green-text-gradient',
+        name: "Pygame",
+        color: "green-text-gradient",
       },
       {
-        name: 'SDLC',
-        color: 'pink-text-gradient',
+        name: "SDLC",
+        color: "pink-text-gradient",
       },
     ],
     info: "",
     image: alien,
-    repo: 'https://github.com/cwharris77/Alien-Invasion',
-    demo: 'https://cwharris77.github.io/Alien-Invasion/',
+    repo: "https://github.com/cwharris77/Alien-Invasion",
+    demo: "https://cwharris77.github.io/Alien-Invasion/",
   },
   {
-    id: 'project-4',
-    name: 'Sudoku',
-    description: 'A Sudoku game built as part of an Agile team using JavaFX',
+    id: "project-4",
+    name: "Sudoku",
+    description: "A Sudoku game built as part of an Agile team using JavaFX",
     tags: [
       {
-        name: 'Java',
-        color: 'blue-text-gradient',
+        name: "Java",
+        color: "blue-text-gradient",
       },
       {
-        name: 'JavaFX',
-        color: 'green-text-gradient',
+        name: "JavaFX",
+        color: "green-text-gradient",
       },
       {
-        name: 'MVC',
-        color: 'pink-text-gradient',
+        name: "MVC",
+        color: "pink-text-gradient",
       },
     ],
     info: "",
     image: sudoku,
-    repo: 'https://github.com/cwharris77/sudoku/tree/55886c9c5e931ae58ac5a63fc34a648056593e31/team-project-segfault',
-    demo: '',
+    repo: "https://github.com/cwharris77/sudoku/tree/55886c9c5e931ae58ac5a63fc34a648056593e31/team-project-segfault",
+    demo: "",
   },
   {
-    id: 'project-5',
-    name: 'Wiki Racer',
+    id: "project-5",
+    name: "Wiki Racer",
     description:
-      'An implementation of the Wiki Ladder game where the challenge is to navigate from a designated start Wikipedia page to a predetermined end page using only hyperlinks within the articles in the quickest time.',
+      "An implementation of the Wiki Ladder game where the challenge is to navigate from a designated start Wikipedia page to a predetermined end page using only hyperlinks within the articles in the quickest time.",
     tags: [
       {
-        name: 'Java',
-        color: 'blue-text-gradient',
+        name: "Java",
+        color: "blue-text-gradient",
       },
       {
-        name: 'Memoization',
-        color: 'green-text-gradient',
+        name: "Memoization",
+        color: "green-text-gradient",
       },
       {
-        name: 'Multi-Threading',
-        color: 'pink-text-gradient',
+        name: "Multi-Threading",
+        color: "pink-text-gradient",
       },
     ],
     info: "",
     image: wiki,
-    repo: 'https://github.com/cwharris77/WikiRacer',
-    demo: '',
+    repo: "https://github.com/cwharris77/WikiRacer",
+    demo: "",
   },
 ];
 
 export { experiences, projects, services, technologies };
-
